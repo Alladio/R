f6da3968c9f8ded45102b308192459342d148ecd
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-22 01:34:27 -0300

    basic matrix

---
+++ matrix.r
@@ -0,0 +1,5 @@
+# Construct a matrix with 3 rows that contain the numbers 1 up to 9
+#The argument byrow indicates that the matrix is filled by the rows. If we want the matrix to be filled by the columns, we just place byrow = FALSE.
+#The third argument nrow indicates that the matrix should have three rows.
+matrix(1:9, byrow=TRUE,nrow = 3)
+
