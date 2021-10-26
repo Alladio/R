45807560ad7646a5fbf2ab94d5c93593d475c019
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-26 19:41:32 -0300

    Summary and Ordered

--- factor.r
+++ factor.r
@@ -7,3 +7,49 @@ factor_sex_vector <- factor(sex_vector)
 
 # Print out factor_sex_vector
 factor_sex_vector
+
+#There are two types of categorical variables: 
+#ONE: a nominal categorical variable 
+#TWO: an ordinal categorical variable.
+
+# Animals
+animals_vector <- c("Elephant", "Giraffe", "Donkey", "Horse")
+factor_animals_vector <- factor(animals_vector)
+factor_animals_vector
+
+# Temperature
+temperature_vector <- c("High", "Low", "High","Low", "Medium")
+factor_temperature_vector <- factor(temperature_vector, order = TRUE, 
+levels = c("Low", "Medium", "High"))
+factor_temperature_vector
+
+#SUMMARY
+#SUMMARY
+
+# Build factor_survey_vector with clean levels
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+summary(survey_vector)
+levels(factor_survey_vector) <- c("Female", "Male")
+factor_survey_vector
+
+# Generate summary for survey_vector
+summary(survey_vector)
+
+# Generate summary for factor_survey_vector
+summary(factor_survey_vector) 
+
+#ORDERED FACTORS
+#ORDERED FACTORS
+
+# Create speed_vector
+speed_vector <- c("medium", "slow", "slow", "medium", "fast")
+
+# Convert speed_vector to ordered factor vector
+factor_speed_vector <- factor(speed_vector,
+                                ordered=TRUE,
+                                levels=c("slow","medium","fast"))
+
+# Print factor_speed_vector
+factor_speed_vector
+summary(factor_speed_vector)
